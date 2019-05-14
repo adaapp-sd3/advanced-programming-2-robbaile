0bb736df2be6c21ea449e724e07ea804b4a859f9
robbaile <robbaile12@outlook.com>
2019-05-14 10:23:40 +0100

    To do - Add grow method for corn/crops

--- src/models/crops/Corn.ts
+++ src/models/crops/Corn.ts
@@ -1,24 +1,20 @@
 import Crop from '../abstract/Crop';
-//import Farm from "../Farm";
 
 class Corn extends Crop {
   name: string = "Corn"
   genus: string = "Corn"
   imgUrl: string = "/img/twtr/1f33d.png"
-  //farm: Farm
-  
-  constructor() {
-    super()
-    //this.farm = farm
-  }
     
-    public preload() {
-      this.p5Img = this.p5.loadImage(this.imgUrl)
-    }
-  
-    public draw(): any {
-      this.constrainItem()
-      this.stopForFarmer()
-    }
+  public preload() {
+    this.p5Img = this.p5.loadImage(this.imgUrl)
   }
-  export default Corn;
+
+  //To do - add a grow method
+
+
+  public draw(): any {
+    this.constrainItem()
+    this.stopForFarmer()
+  }
+}
+export default Corn;
