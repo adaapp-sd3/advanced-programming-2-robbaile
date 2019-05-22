2ef015fec2c3d2358737070462349a9fc4b67619
robbaile <robbaile12@outlook.com>
2019-05-22 08:39:53 +0100

    updated README.md

--- README.md
+++ README.md
@@ -22,21 +22,25 @@ Hour 7
 Hour 8 
 - Added instructions page and styled home page. This included drawing SVG icon for the homescreen
 
-Hour 9  
+Hour 9 & 10 
 - Added a weather API and rendered out a weather dashboard. Location initially hardcoded. 
 - Started adding a text input on homepage that will update the weather based on where you are.  
 
-Hour 10
+Hour 11
 - Found solution for setting location using localStorage as state was refreshing
 
-Hour 11 
+Hour 12 & 13
 - Added corn/crop classes and added the ability to plant crops in the farm if a field is empty
 
-Hour 12 
+Hour 14 & 15
 - Added the ability to harvest the crops when the average age of them is over 30 weeks
 
-Hour 13 
+Hour 16 & 17 
 - Added a level system where you progress levels each time you double your income 
 
-Hour 14 
+Hour 18
 - Updated grow method and changed some css so that level dashboard will work
+
+Hour 19 
+- Enabled ability to sell corn and tested the UI to see if it all works in the way intended
+- Looked at potential last additions
--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -5,8 +5,19 @@ class Instructions extends Component {
   render() {
     return (
       <div className="Instructions">
-        <p>This game starts with you having a field of cows, a field of sheep and a field of chickens</p>
-        <p>From cows you can get milk, from sheep you can get wool and from chickens you can get eggs which can all be sold at the marketplace.</p>
+        <h1>Instructions</h1>
+
+        <p>This simple farming game enables you to run your own arable and livestock farm and make money from it</p>
+        <h2>The game</h2>
+        <p>To begin you will have £1000, cows, chickens and sheep</p>
+        <p>You can milk cows, shear sheep and collect eggs from chickens</p>
+        <p>You can also go into the marketplace and buy seeds to plant in your empty fields.</p>
+        <p>Sell your animals if you want to have a full arable farm.</p>
+
+        <h2>Level up</h2>
+        <p>To level up all you need to do is double your net worth!</p>
+        <p>Remember to put in your location to get weather updates for your farm!</p>
+        <p>Good luck</p>
         <a href="/">return to home</a>
       </div>
     )
